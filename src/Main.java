e723ddb6f391846572cd750e74213b77287201bf
SGDoubler <doublesn@mail.uc.edu>
2023-06-11 13:52:30 -0400

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double ccBalance = 5000.00;
+        double intRate = .17;
+
+        double initialInterest = ccBalance * intRate;
+        double noPayments = ccBalance + initialInterest;
+        double nextMonth = (ccBalance + noPayments) * intRate;
+
+
+        System.out.println("The initial interest due is " + initialInterest);
+        System.out.println("The total amount due is " + noPayments);
+        System.out.println("If no payment is made the total interest due is " + nextMonth);
+
+
     }
 }
